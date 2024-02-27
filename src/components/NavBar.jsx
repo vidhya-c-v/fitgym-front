f177a168f1397e8156dbdcabd0e3ad15fe5f12d9
krishnadam206 <krishnadasm206@gmail.com>
2024-02-27 13:50:06 +0530

    Trainerlogin Committed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,27 +7,27 @@ const NavBar = () => {
 
 <nav className="navbar navbar-expand-lg bg-danger">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">
+    <Link className="navbar-brand" to="#">
     <img src="https://us.123rf.com/450wm/imogi/imogi1805/imogi180500240/102169959-vintage-fitness-concept.jpg?ver=6" alt="Bootstrap" width="100" height="80" />
                         <font  color="white"><b> &nbsp;&nbsp;FitGym</b></font>
 
-    </a>
+    </Link>
   
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/"><font  color="white">Home</font></a>
+          <Link className="nav-link active" aria-current="page" to="/"><font  color="white">Home</font></Link>
         </li>
         
         
         
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           <font  color="white"> Login</font>
-          </a>
+          </Link>
           <ul className="dropdown-menu">
             <li><Link className="dropdown-item" to="#">Admin login</Link></li>
-            <li><Link className="dropdown-item" to="#">Trainer login</Link></li>
+            <li><Link className="dropdown-item" to="/trainerLogin">Trainer login</Link></li>
             <li><Link className="dropdown-item" to="#">User login</Link></li>
           </ul>
         </li>
