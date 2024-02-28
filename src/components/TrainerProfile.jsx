c2643303c2d86d914eae9154fc824115d5bd3c5c
Melvinpml <melvinpoulose06@gmail.com>
2024-02-28 10:34:26 +0530

    model of view profile trainer

---
+++ src/components/TrainerProfile.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TrainerProfile = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <div class="card border-dark mb-3" >
+                            <div class="card-header">Profile</div>
+                            <div class="card-body">
+                                <h2 class="card-title">Name</h2>
+                                <h5 class="card-text">E-mail</h5>
+                                <h5 class="card-text">Age</h5>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TrainerProfile
