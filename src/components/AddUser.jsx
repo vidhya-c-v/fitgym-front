a3d0ee8ede807b09bec454baf3fcfabb407f33d7
krishnadam206 <krishnadasm206@gmail.com>
2024-02-28 18:04:02 +0530

    Committed

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminNavBar from './AdminNavBar'
+import axios, { Axios } from 'axios'
 
 const AddUser = () => {
+    const [input, setInput] = new useState(
+        {
+            name: "",
+            place: "",
+            age: "",
+            height: "",
+            weight: "",
+            bloodGroup: "",
+            email: "",
+            password: "",
+            packageId:""
+
+
+        }
+    )
+
+    const inputHandler = (event) => {
+        setInput({ ...input, [event.target.name]: event.target.value })
+    }
+
+    const readValues = () => {
+        axios.post("http://localhost:3001/api/member/addmember", input).then(
+            (response) => {
+                if (response.data.status == "success") {
+
+                    alert("success")
+
+                    setInput({
+
+                        name: "",
+                        place: "",
+                        age: "",
+                        height: "",
+                        weight: "",
+                        bloodGroup: "",
+                        email: "",
+                        password: "",
+                        packageId:""
+                    })
+
+                }
+                else {
+
+                    alert("failed to add")
+
+                }
+            }
+        )
+    }
     return (
         <div>
             <AdminNavBar />
@@ -11,64 +61,58 @@ const AddUser = () => {
                 <div className="row g-3">
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                        <label htmlFor="" className="form-label">MemberId</label>
-                        <input type="text" className="form-control" name="packageName" />
-
-                    </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
                         <label htmlFor="" className="form-label">Name</label>
-                        <input type="text" className="form-control" name="packageName" />
+                        <input type="text" className="form-control" name="name" value={input.name} onChange={inputHandler} />
 
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                         <label htmlFor="" className="form-label">Place</label>
-                        <input type="text" className="form-control" name="packageDes" />
+                        <input type="text" className="form-control" name="place" value={input.place} onChange={inputHandler} />
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                         <label htmlFor="" className="form-label">Age</label>
-                        <input type="text" className="form-control" name="packageAmount" />
+                        <input type="text" className="form-control" name="age" value={input.age} onChange={inputHandler} />
 
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                         <label htmlFor="" className="form-label">Height</label>
-                        <input type="text" className="form-control" name="packageAmount" />
+                        <input type="text" className="form-control" name="height" value={input.height} onChange={inputHandler} />
 
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                         <label htmlFor="" className="form-label">Weight</label>
-                        <input type="text" className="form-control" name="packageAmount" />
+                        <input type="text" className="form-control" name="weight" value={input.weight} onChange={inputHandler} />
 
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                        <label htmlFor="" className="form-label">Blood groop</label>
-                        <input type="text" className="form-control" name="packageAmount" />
+                        <label htmlFor="" className="form-label">Blood group</label>
+                        <input type="text" className="form-control" name='bloodGroup' value={input.bloodGroup} onChange={inputHandler} />
 
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                        <label htmlFor="" className="form-label">PackageId</label>
-                        <input type="password" className="form-control" name="packageAmount" />
+                        <label htmlFor="" className="form-label">Package Id</label>
+                        <input type="text" className="form-control" name="packageId" value={input.packageId} onChange={inputHandler} />
 
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                         <label htmlFor="" className="form-label">Email</label>
-                        <input type="text" className="form-control" name="packageAmount" />
+                        <input type="text" className="form-control" name="email" value={input.email} onChange={inputHandler} />
 
                     </div>
                     <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                         <label htmlFor="" className="form-label">Password</label>
-                        <input type="password" className="form-control" name="packageAmount" />
+                        <input type="password" className="form-control" name="password" value={input.password} onChange={inputHandler} />
 
                     </div>
-                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    {/* <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                         <label htmlFor="" className="form-label">Registraion Fees Paid</label>
 
                         <div class="form-check">
@@ -83,28 +127,10 @@ const AddUser = () => {
                                 NO
                             </label>
                         </div>
-                    </div>
-
-                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label htmlFor="" className="form-label">Package Fees Paid</label>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked />
-                            <label class="form-check-label" for="exampleRadios1">
-                                YES
-                            </label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" />
-                            <label class="form-check-label" for="exampleRadios2">
-                               NO
-                            </label>
-                        </div>
-                        
-                    </div>
-                    
+                    </div> */}
 
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                       <center> <button className="btn btn-primary" >Add Member</button></center>
+                        <center> <button className="btn btn-primary" onClick={readValues} >Add Member</button></center>
                     </div>
                 </div>
             </div>
