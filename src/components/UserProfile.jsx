c09fc21d282638046ce19e5d96951bbb0dd86811
vidhya-c-v <vidhyacv14@gmail.com>
2024-03-05 17:37:33 +0530

    newpackage

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,67 +1,129 @@
 import axios from 'axios';
-import React from 'react'
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import UserNavBar from './UserNavBar';
-import { useEffect } from 'react';
 
 const UserProfile = () => {
-    const [input, setInput] = new useState(
-        {
-            _id: sessionStorage.getItem("userid")
-        }
-    )
     const [output, setOutput] = useState([]);
+    const [allPackages, setAllPackages] = useState([]);
+    const [selectedPackageId, setSelectedPackageId] = useState(null);
 
-    const readValues = () => {
-        console.log(input)
-        axios.post("http://localhost:3001/api/member/viewmemberdetails", input,{headers:{token:sessionStorage.getItem("token")}}).then((response) => {
-            setOutput(response.data)
-            console.log(response.data)
+    const fetchUserDetails = () => {
+        axios.post("http://localhost:3001/api/member/viewmemberdetails", { _id: sessionStorage.getItem("userid") }, { headers: { token: sessionStorage.getItem("token") } })
+            .then((response) => {
+                setOutput(response.data);
+                setSelectedPackageId(response.data[0]?.package_id);
+            })
+            .catch(error => {
+                console.error("Error fetching user details:", error);
+            });
+    };
 
-        })
-    }
-    useEffect(() => { readValues() }, [])
+    const fetchAllPackages = () => {
+        axios.get("http://localhost:3001/api/package/viewallpackage")
+            .then((response) => {
+                setAllPackages(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching all packages:", error);
+            });
+    };
+
+    useEffect(() => {
+        fetchUserDetails();
+        fetchAllPackages();
+    }, []); // Run once on component mount
+
+    // Update user profile after package is successfully updated
+    const updateUserProfile = () => {
+        fetchUserDetails();
+    };
+
+    // Function to handle package selection
+    const handlePackageSelection = (packageId) => {
+        setSelectedPackageId(packageId);
+        updateSelectedPackage(packageId);
+    };
+
+    // Function to update selected package for the user
+    const updateSelectedPackage = (packageId) => {
+        axios.post("http://localhost:3001/api/member/updatepackage", { userId: sessionStorage.getItem("userid"), packageId }, { headers: { token: sessionStorage.getItem("token") } })
+            .then((response) => {
+                console.log("Selected package updated successfully");
+                updateUserProfile(); // Fetch updated user details after package update
+            })
+            .catch(error => {
+                console.error("Error updating selected package:", error);
+            });
+    };
+
     return (
         <div className='homebg'>
-            <UserNavBar/>
-            <br>
-            </br>
-            <br></br>
+            <UserNavBar />
+            <br />
+            <br />
 
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        {/* Display user details */}
+                        <div className="card border-dark mb-3">
+                            <div className="card-header">Profile</div>
+                            <div className="card-body">
+                                {output.map((user, index) => (
+                                    <React.Fragment key={index}>
+                                        <h2 className="card-title">Name: {user.name}</h2>
+                                        <h5 className="card-text">Place : {user.place}</h5>
+                                        <h5 className="card-text">Age : {user.age}</h5>
+                                        <h5 className="card-text">Height: {user.height}</h5>
+                                        <h5 className="card-text">Weight: {user.weight}</h5>
+                                        <h5 className="card-text">Bloodgroup: {user.bloodGroup}</h5>
+                                        <h5 className="card-text">Join date: {user.registerDate}</h5>
+                                        <h5 className="card-text">Due Amount : {user.dueAmount}</h5>
+                                        <h5 className="card-text">Remaining days for due : {user.remainingDaysForNextDue}</h5>
+                                        {/* Display selected package details */}
+                                        <div className="card border-dark mb-3">
+                                            <div className="card-header">Selected Package</div>
+                                            <div className="card-body">
+                                                {allPackages.map(pkg => {
+                                                    if (pkg._id === selectedPackageId) {
+                                                        return (
+                                                            <React.Fragment key={pkg._id}>
+                                                                <h5 className="card-title">Package Name: {pkg.packageName}</h5>
+                                                                <p className="card-text">Package Description: {pkg.packageDes}</p>
+                                                                <p className="card-text">Package Amount: {pkg.packageAmount}</p>
+                                                            </React.Fragment>
+                                                        );
+                                                    }
+                                                    return null;
+                                                })}
+                                            </div>
+                                        </div>
+                                    </React.Fragment>
+                                ))}
+                            </div>
+                        </div>
 
-                        {
-                            output.map(
-                                (value,index)=>{
-                                    return <div class="card border-dark mb-3" >
-                                    <div class="card-header">Profile</div>
-                                    <div class="card-body">
-                                        <h2 class="card-title">Name: {value.name}</h2>
-                                        <h5 class="card-text">Place : {value.place}</h5>
-                                        <h5 class="card-text">Age : {value.age}</h5>
-                                        <h5 class="card-text">Height : {value.height}</h5>
-                                        <h5 class="card-text">Weight : {value.weight}</h5>
-                                        <h5 class="card-text">Blood Group :{value.bloodGroup}</h5>
-                                        <h5 class="card-text">Email : {value.email}</h5>
-                                        <h5 class="card-text">Joined Date : {value.registerDate}</h5>
-                                        <h5 class="card-text">Package Name : {value.package_name}</h5>
-                                        <h5 class="card-text">Package Amount : {value.package_amount}</h5>
-                                        <h5 class="card-text">Due Amount : {value.dueAmount}</h5>
-                                        <h5 class="card-text">Remaining days for due : {value.remainingDaysForNextDue}</h5>        
+                        {/* Display all available packages */}
+                        <h2><font color="white">All Available Packages</font></h2>
+                        <div className="row">
+                            {allPackages.map((pkg, index) => (
+                                <div key={index} className="col-4">
+                                    <div className={`card border-dark mb-3 ${pkg._id === selectedPackageId ? 'selected-package' : ''}`}>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{pkg.packageName}</h5>
+                                            <p className="card-text">{pkg.packageDes}</p>
+                                            <p className="card-text">{pkg.packageAmount}</p>
+                                            <button onClick={() => handlePackageSelection(pkg._id)}>Select</button>
+                                        </div>
                                     </div>
                                 </div>
-                                }
-                            )
-                        }
-
+                            ))}
+                        </div>
                     </div>
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default UserProfile
+export default UserProfile;
--- src/components/UserUpdatePackage.jsx
+++ src/components/UserUpdatePackage.jsx
@@ -1,63 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import AdminNavBar from './AdminNavBar'; // Import your AdminNavBar component
 import UserNavBar from './UserNavBar';
 
 const UserUpdatePackage = () => {
-    const [data, setData] = useState([]);
     const [input, setInput] = useState({
-        _id: sessionStorage.getItem("userid"),
-        email :"",
+        email: "",
         packagename: ""
-    })
-
-
-    const getData = () => {
-        axios.get("http://localhost:3001/api/package/viewallpackage")
-            .then((response) => {
-                setData(response.data);
-            })
-            .catch((error) => {
-                console.error("Error fetching data:", error);
-            })
-    }
-
-    const getEmail = ()=>{
-        axios.get("http://localhost:3001/api/member/viewmemberdetails", input._id).then(
-            (response)=>{
-                setInput(prevState =>({...prevState,email:response.data.email}))
-            }
-        )
-    }
-
-    useEffect(() => {
-        getData()
-    }, [])
+    });
 
     const inputHandler = (event) => {
-       setInput({...input,[event.target.name]:event.target.value})
-    }
+        setInput({ ...input, [event.target.name]: event.target.value });
+    };
 
-    const readValues = () => {
-        if (!input._id) {
-            alert("User ID is missing")
-            return
+    const updateUserPackage = async () => {
+        if (!input.email || !input.packagename) {
+            alert("Please provide email and package name");
+            return;
         }
 
-        const updateData = {
-            userId: input._id,
-            packagename: input.packagename
-        };
+        try {
+            const response = await axios.post("http://localhost:3001/api/package/updatepackage", input);
+            console.log(response.data);
+            alert("Updated package successfully");
+            // After updating the package, fetch user details to reflect changes in the user profile
+            fetchUserDetails(input.email);
+        } catch (error) {
+            console.error("Error updating package:", error);
+        }
+    };
 
-        axios.post("http://localhost:3001/api/package/updatepackage", updateData)
-            .then((response) => {
-                console.log(response.data);
-                alert("updated package successfully")
-            })
-            .catch((error) => {
-                console.error("Error updating package:", error);
-            })
-    }
+    const fetchUserDetails = async (email) => {
+        try {
+            const response = await axios.post("http://localhost:3001/api/member/viewmemberdetails", { email });
+            // Handle the response to update user profile details in the frontend
+            console.log("User details:", response.data);
+            // You can set state or update the user profile directly here
+        } catch (error) {
+            console.error("Error fetching user details:", error);
+        }
+    };
 
     return (
         <div>
@@ -69,37 +50,33 @@ const UserUpdatePackage = () => {
                         <h1>Update Package</h1>
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <label htmlFor="" className="form-label">User</label>
+                        <label htmlFor="" className="form-label">User Email</label>
                         <input
-                            type="text"
+                            type="email"
                             className="form-control"
-                            value={input._id}
+                            name="email"
+                            value={input.email}
                             onChange={inputHandler}
-                            disabled
                         />
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                         <label htmlFor="" className="form-label">Package Name</label>
-                        <select className="form-control" name="packagename" id="" onChange={inputHandler}>
-                            <option>--Select package--</option>
-                            {
-                                data.map(
-                                    (value, index) => {
-                                        return <option value={value.packageName} >{value.packageName}</option>
-
-                                    }
-                                )
-                            }
-                        </select>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="packagename"
+                            value={input.packagename}
+                            onChange={inputHandler}
+                        />
                     </div>
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <button className="btn btn-info" onClick={readValues}>Update</button>
+                        <button className="btn btn-info" onClick={updateUserPackage}>Update</button>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default UserUpdatePackage;
